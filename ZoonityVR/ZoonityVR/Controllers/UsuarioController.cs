71e7572cb95425c2645c09c1f97f3d6e071944cf
AntonioCarlosOrdonez_MainHomePC <carlossant791@hotmail.com>
2018-04-12 20:56:53 +0200

    Creacion del controller UsuarioController

---
+++ ZoonityVR/ZoonityVR/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ZoonityVR.Models;
+
+namespace ZoonityVR.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        Usuario[] usuarios = new Usuario[]
+        {
+            new Usuario{Nombre = "Laura" , GallineroAreaCompletado = false, ElefanteAreaCompletado = false, GirafaAreaCompletado = false, LeonesAreaCompletado = false, CocodrilosAreaCompletado = false, SimiosAreaCompletado = false, FocasAreaCompletado = false, DelfinesAreaCompletado = false },
+            new Usuario{Nombre = "Carlos" , GallineroAreaCompletado = false, ElefanteAreaCompletado = false, GirafaAreaCompletado = false, LeonesAreaCompletado = false, CocodrilosAreaCompletado = false, SimiosAreaCompletado = false, FocasAreaCompletado = false, DelfinesAreaCompletado = false }
+        };
+
+        // URI: /api/usuarios
+        public IEnumerable<Usuario> GetAllUsuarios()
+        {
+            return usuarios;
+        }
+
+        // URI: /api/usuarios/nombre
+        public IHttpActionResult GetUsuario(string nombre)
+        {
+            var usuario = usuarios.FirstOrDefault((u) => u.Nombre == nombre);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return Ok(usuario);
+        }
+
+    }
+}
---
+++ ZoonityVR/ZoonityVR/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZoonityVR.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public bool GallineroAreaCompletado { get; set; }
+        public bool ElefanteAreaCompletado { get; set; }
+        public bool GirafaAreaCompletado { get; set; }
+        public bool LeonesAreaCompletado { get; set; }
+        public bool CocodrilosAreaCompletado { get; set; }
+        public bool SimiosAreaCompletado { get; set; }
+        public bool FocasAreaCompletado { get; set; }
+        public bool DelfinesAreaCompletado { get; set; }
+    }
+}
